0faa43adeb9311121b7ddcaecf7a230bfeb01feb
Rafajel29 <52860634+Rafajel29@users.noreply.github.com>
2019-11-13 19:03:12 -0500

    Add files via upload

---
+++ Backend/udpsendrecieve.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+import socket
+import sys
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('192.168.1.157', 2390)
+#message = raw_input("Enter Relay Codoe: ")
+
+try:
+    while(1):
+        message = raw_input("Enter Relay Codoe: ")
+        # Send data
+        print >>sys.stderr, 'sending "%s"' % message
+        sent = sock.sendto(message, server_address)
+    
+        # Receive response
+        print >>sys.stderr, 'waiting to receive'
+        data, server = sock.recvfrom(4096)
+        print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
